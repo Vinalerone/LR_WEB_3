afc888fc6926fab185034b6a1bc8722bb83b4c1c
Vinalerone <90629249+Vinalerone@users.noreply.github.com>
2024-07-25 12:54:06 +0000

    ЛР 4 начала

--- LR3/LR4/main.js
+++ LR3/LR4/main.js
@@ -1,15 +1,3 @@
-// async function process() {
-//   let r = await fetch("data.json");
-//   let data = await r.json();
-//  // console.log(data) // выведем исходный список
-  
-//   let mappedData = data.map(item => {
-//       return item;
-//   }) // мэпю
-//  // console.log(mappedData)  // выведем отображенный список
-// }
-// process()
-//кнопка 2
 async function process2() {
   let r = await fetch("data.json");
   let data = await r.json();
@@ -98,10 +86,14 @@ async function fillList() {
   let r = await fetch("data.json");
   let data = await r.json();
 
+
+
 let container = document.querySelector("#elements-container > tbody");
 //console.log(container)
 let selectPoleznost = document.querySelector("#selectPoleznost");
 let  filteredDataFromData = data.filter(item => item["Насколько курс был полезен?"] == selectPoleznost.value);
+
+
 
 container.replaceChildren() //удаляет строчки таблицы
 //console.clear();
@@ -109,25 +101,45 @@ container.replaceChildren() //удаляет строчки таблицы
 let filteredData = data.filter(item => {
   if (selectPoleznost.value == 'не важно') {
    //console.clear();
-    console.log(selectPoleznost.value+'1');
-    container = document.querySelector("#elements-container > tbody");
+    //console.log(selectPoleznost.value+'1');
+   // container = document.querySelector("#elements-container > tbody");
     //console.log(container)
-    selectPoleznost = document.querySelector("#selectPoleznost");
-    console.log(selectPoleznost.value+'1.1');
+  //selectPoleznost = document.querySelector("#selectPoleznost");
+    //console.log(selectPoleznost.value+'1.1');
     //item['Насколько курс был полезен?'] == selectPoleznost.value;
-    console.log(item['Насколько курс был полезен?'] );
+    //console.log(item['Насколько курс был полезен?'] );
     //<td>${item['Насколько курс был полезен?']}</td>
     //item['Насколько курс был полезен?'] == selectPoleznost.value;
     //selectPoleznost.value = item['Насколько курс был полезен?']; //меняет значение на странице, ХDD
-    console.log(selectPoleznost.value+'1.3');
+    //console.log(selectPoleznost.value+'1.3');
     filteredDataFromData = data.filter(item => item["Насколько курс был полезен?"] != selectPoleznost.value);
     //console.log(filteredDataFromData.value+'1.4');
-    console.log(selectPoleznost.value+'1.5');
-    console.clear();
+    //console.log(selectPoleznost.value+'1.5');
+   // console.clear();
   }
-  else{
-  console.log(selectPoleznost.value+'2');}
+  //else{
+  //console.log(selectPoleznost.value+'2');}
 })
+
+let poleznost = filteredDataFromData.map(item => item['Насколько курс был полезен?']);
+console.log("Значения в столбике полезность:")
+console.log(poleznost)
+
+let poleznostCounters = {} // заводим пустой словарик
+poleznost.forEach(item => { // с помощью forEach, обходим элементы списка
+    // poleznostCounters[item] | 0 -- это хитрая фиговина 
+    // которая вернет значение poleznostCounters[item] если ключ item есть в словарике poleznostCounters
+    // и 0 если ключа item нет в словарике poleznostCounters
+    // ну а "+1" это чтобы увеличивать предыдущее значение ключа на 1, когда встречаем очередное значение
+    // то есть когда встретим первый раз то положится значение  1
+    poleznostCounters[item] = (poleznostCounters[item] | 0) + 1
+})
+// ну и выводим
+console.log(poleznostCounters)
+
+let poleznostStatsNode = document.querySelector("#poleznostStats .stats")
+poleznostStatsNode.innerText = JSON.stringify(poleznostCounters);
+
 
 let container2 = document.querySelector("#elements-container > tbody");
 let selectPoleznost2 = document.querySelector("#selectPoleznost2");
@@ -138,18 +150,37 @@ container2.replaceChildren()
 let filteredData2 = data.filter(item => {
   if (selectPoleznost2.value == 'не важно') {
    //console.clear();
-    console.log(selectPoleznost2.value+'1');
+    //console.log(selectPoleznost2.value+'1');
     container2 = document.querySelector("#elements-container > tbody");
     selectPoleznost2 = document.querySelector("#selectPoleznost2");
-    console.log(selectPoleznost2.value+'1.1');
-    console.log(item['Насколько доволен форматом обучения?'] );
-    console.log(selectPoleznost2.value+'1.3');
+   // console.log(selectPoleznost2.value+'1.1');
+   // console.log(item['Насколько доволен форматом обучения?'] );
+   // console.log(selectPoleznost2.value+'1.3');
     filteredDataFromData2 = data.filter(item => item["Насколько доволен форматом обучения?"] != selectPoleznost2.value);
-    console.log(selectPoleznost2.value+'1.5');
+   // console.log(selectPoleznost2.value+'1.5');
   }
-  else{
-  console.log(selectPoleznost2.value+'2');}
+  //else{
+  //console.log(selectPoleznost2.value+'2');}
 })
+
+let poleznost2 = filteredDataFromData2.map(item => item['Насколько доволен форматом обучения?']);
+console.log("Значения в столбике полезность:")
+console.log(poleznost2)
+
+let poleznostCounters2 = {} // заводим пустой словарик
+poleznost2.forEach(item => { // с помощью forEach, обходим элементы списка
+    // poleznostCounters[item] | 0 -- это хитрая фиговина 
+    // которая вернет значение poleznostCounters[item] если ключ item есть в словарике poleznostCounters
+    // и 0 если ключа item нет в словарике poleznostCounters
+    // ну а "+1" это чтобы увеличивать предыдущее значение ключа на 1, когда встречаем очередное значение
+    // то есть когда встретим первый раз то положится значение  1
+    poleznostCounters2[item] = (poleznostCounters2[item] | 0) + 1
+})
+// ну и выводим
+console.log(poleznostCounters2)
+
+let poleznostStatsNode2 = document.querySelector("#poleznostStats2 .stats")
+poleznostStatsNode2.innerText = JSON.stringify(poleznostCounters2);
 
 let container3 = document.querySelector("#elements-container > tbody");
 let selectPoleznost3 = document.querySelector("#selectPoleznost3");
@@ -162,6 +193,27 @@ let filteredData3 = data.filter(item => {
     filteredDataFromData3 = data.filter(item => item["Отметь, в какой мере ты удовлетворен курсом?"] != selectPoleznost3.value);
   }
 })
+
+let poleznost3 = filteredDataFromData3.map(item => item['Отметь, в какой мере ты удовлетворен курсом?']);
+console.log("Значения в столбике полезность:")
+console.log(poleznost3)
+
+let poleznostCounters3 = {} // заводим пустой словарик
+poleznost3.forEach(item => { // с помощью forEach, обходим элементы списка
+    // poleznostCounters[item] | 0 -- это хитрая фиговина 
+    // которая вернет значение poleznostCounters[item] если ключ item есть в словарике poleznostCounters
+    // и 0 если ключа item нет в словарике poleznostCounters
+    // ну а "+1" это чтобы увеличивать предыдущее значение ключа на 1, когда встречаем очередное значение
+    // то есть когда встретим первый раз то положится значение  1
+    poleznostCounters3[item] = (poleznostCounters3[item] | 0) + 1
+})
+// ну и выводим
+console.log(poleznostCounters3)
+
+let poleznostStatsNode3 = document.querySelector("#poleznostStats3 .stats")
+poleznostStatsNode3.innerText = JSON.stringify(poleznostCounters3);
+
+
 
 
 //Конкатенация двух массивов (вывод 2 массива одновременно ._.)
